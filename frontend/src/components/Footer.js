1ddbed196fa77a724a20cd76942cd0c6badf3380
DownTheMatrix <suija87@gmail.com>
2020-10-09 17:56:17 +0200

    Add styles to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,25 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  footerWrapper: {
+    backgroundColor: "lightblue",
+  },
+  footerContent: {
+    padding: "1rem",
+  },
+}));
 
 function Footer() {
+  const classes = useStyles();
   return (
-    <footer>
-      <Container>
-        <Typography align="center" variant="h6">
+    <footer className={classes.footerWrapper}>
+      <CssBaseline />
+      <Container className={classes.footerContent}>
+        <Typography align="center" component="p">
           Copyright 2020 &copy; ArtShop
         </Typography>
       </Container>
