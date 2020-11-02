4bcb262c515db0df20f0b475f3deeb4be9f9fa03
DownTheMatrix <suija87@gmail.com>
2020-11-02 12:48:16 +0100

    Convert commonJS to ES6 modules syntax

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const dotenv = require("dotenv")
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
-const app = express()
+const app = express();
 dotenv.config();
 
 app.get("/", (req, res) => {
@@ -21,4 +21,7 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV;
 
-app.listen(PORT, console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`)
+);
