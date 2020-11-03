142d41f0d6b0ccbb334f76ed2d42a7aeb9fe6b64
DownTheMatrix <suija87@gmail.com>
2020-11-03 12:28:28 +0100

    Add styles to Loader component

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -5,14 +5,14 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    "& > * + *": {
-      marginLeft: theme.spacing(2),
-    },
+    justifyContent: "center",
+    alignItems: "center",
   },
 }));
 
 const Loader = () => {
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
       <CircularProgress color="primary" />
