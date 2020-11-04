c52f245750ee2b778154c73859b787aabcf6adde
DownTheMatrix <suija87@gmail.com>
2020-11-04 14:26:22 +0100

    Style page

--- frontend/src/screens/PageNotFound.js
+++ frontend/src/screens/PageNotFound.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+import { Container, Typography, Box, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import pageNotFoundIllustration from "../images/page-not-found.png";
+import ButtonLink from "../components/ButtonLink";
+import { Link as RouterLink } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1.5rem",
+    },
+  },
+  wrapper: {
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  responsiveImg: {
+    maxWidth: "100%",
+    margin: "auto",
+    height: "auto",
+    display: "block",
+  },
+}));
 
 const PageNotFound = () => {
-    return (
-        <div>
-            404: Page Not Found
-        </div>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Container>
+      <Grid container className={classes.wrapper}>
+        <Grid item xs>
+          <Typography
+            className={classes.heading}
+            align="center"
+            variant="h3"
+            component="h1"
+          >
+            Page not found.
+          </Typography>
+        </Grid>
+        <Grid item xs>
+          <ButtonLink
+            style={{minWidth: "250px !important"}}
+            component={RouterLink}
+            to="/"
+            variant="contained"
+            color="secondary"
+          >
+            Go Back
+          </ButtonLink>
+        </Grid>
+      </Grid>
+      <Box mt={5}>
+        <figure>
+          <img
+            className={classes.responsiveImg}
+            src={pageNotFoundIllustration}
+            alt="404 page not found illustration"
+          />
+        </figure>
+      </Box>
+    </Container>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
